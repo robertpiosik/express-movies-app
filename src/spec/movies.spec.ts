aeb8c98749a435ff3d8fc56013f4c6d765a05454
Robert Piosik <robertpiosik@gmail.com>
2019-08-31 21:59:02 +0200

    get movies endpoint fail test

--- src/spec/movies.spec.ts
+++ src/spec/movies.spec.ts
@@ -1,11 +1,15 @@
 import request, { Response } from "supertest";
 import app from "../app";
 
-describe("GET /api/v1/movies", () =>{
-	it('should return 200', ()=>{
-		return 
-	})
-})
+describe("GET /api/v1/movies", () => {
+	it("should return 200", () => {
+		return request(app)
+			.get("/api/v1/movies")
+			.expect((res: Response) => {
+				expect(res.status).toBe(200);
+			});
+	});
+});
 
 // describe("POST /api/v1/movies", () => {
 // 	it("should return 422 without Authorization header", () => {
@@ -16,4 +20,3 @@ describe("GET /api/v1/movies", () =>{
 // 			});
 // 	});
 // });
-
