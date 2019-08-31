d2d2bc332bcde85d2c7794f25e3ef28855ffce6a
Robert Piosik <robertpiosik@gmail.com>
2019-08-31 23:09:21 +0200

    pass test 'should return 404 if no movie was found for given title in external database'

--- src/controllers/movies.ts
+++ src/controllers/movies.ts
@@ -1,10 +1,15 @@
 import { Request, Response, NextFunction } from "express";
+import fetch from "node-fetch";
 
 export const getMovies = (req: Request, res: Response, next: NextFunction) => {
 	res.sendStatus(200);
 };
 
-export const postMovies = (req: Request, res: Response, next: NextFunction) => {
+export const postMovies = async (
+	req: Request,
+	res: Response,
+	next: NextFunction
+) => {
 	if (!req.isAuth) {
 		return next({
 			status: 401,
@@ -14,6 +19,7 @@ export const postMovies = (req: Request, res: Response, next: NextFunction) => {
 	}
 
 	const { title } = req.body;
+
 	if (!title) {
 		return next({
 			status: 422,
@@ -21,5 +27,21 @@ export const postMovies = (req: Request, res: Response, next: NextFunction) => {
 			message: "Title is missing."
 		});
 	}
+
+	const omdbEndpoint = encodeURI(
+		`http://www.omdbapi.com/?t=${title}&apikey=${process.env.OMDB_API_KEY}`
+	);
+
+	const movieDataResponse = await fetch(omdbEndpoint);
+	const movieData = await movieDataResponse.json();
+	
+	if (movieData.Response === "False") {
+		return next({
+			status: 404,
+			name: "MovieNotFound",
+			message: "Movie in external database was not found."
+		});
+	}
+
 	res.send(200);
 };
