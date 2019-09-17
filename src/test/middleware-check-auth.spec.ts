7e5ac8f05b8ebb0dc823b415afd2ecabf8ae8914
Robert Piosik <robertpiosik@gmail.com>
2019-09-18 00:32:06 +0200

    tests

---
+++ src/test/middleware-check-auth.spec.ts
@@ -0,0 +1,18 @@
+import checkAuth from "../middleware/check-auth";
+
+describe("middleware checkAuth", () => {
+	it("should return an error if no auth header is present", done => {
+		const req: any = {
+			get() {
+				return null;
+			}
+		};
+		const res:any = null;
+		checkAuth(req, res, error => {
+			if (error.message === "Not authorized.") done();
+			return new Error(
+				"Expected to return error with message: Not authorized."
+			);
+		});
+	});
+});
